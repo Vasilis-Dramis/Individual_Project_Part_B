e553a1a0dc92212340e7bfdc5d011aa713ceb5d0
Vasilis-Dramis <80609098+Vasilis-Dramis@users.noreply.github.com>
2021-06-04 00:05:01 +0100

    Update AssemblyInfo.cs

--- IndividualProjectPartB/IndividualProjectPartB/Properties/AssemblyInfo.cs
+++ IndividualProjectPartB/IndividualProjectPartB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IndividualProjectPartB_GeorgeMalandris")]
+[assembly: AssemblyTitle("IndividualProjectPartB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IndividualProjectPartB_GeorgeMalandris")]
+[assembly: AssemblyProduct("IndividualProjectPartB")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
